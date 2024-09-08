68b61130ec5c95453632173a823c879e8922e33f
VishwRana-Coder <vishw.rana.k@gmail.com>
2024-09-08 16:33:13 +0530

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="flex w-full justify-center h-[80vh] items-center">
+      <h1 className="text-5xl flex text-center">
+        Welcome to Swinter
+      </h1>
+    </div>
+  );
+}
